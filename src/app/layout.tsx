5dd5fad0a72ad226e150ca7a1d6e946d99910c96
Rifky Alfarez <207006052@student.unsil.ac.id>
2024-05-09 10:35:40 +0700

    feat: add not found page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${inter.className} bg-gray-50 dark:bg-gray-950`}>
         <Providers>
           <Navbar />
---
+++ src/app/not-found.tsx
@@ -0,0 +1,29 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function NotFound() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="flex items-center backdrop-blur-xl p-8 rounded-xl">
+        <div>
+          <Image
+            src="/images/not-found.png"
+            alt="404"
+            width={300}
+            height={300}
+            priority
+          />
+        </div>
+        <div>
+          <h2 className="text-xl md:text-4xl font-bold">404 Not Found</h2>
+          <p className="text-sm md:text-xl mb-8">
+            The page you are looking for does not exist
+          </p>
+          <Link href="/" className="underline text-xs md:text-base">
+            Return Home
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}

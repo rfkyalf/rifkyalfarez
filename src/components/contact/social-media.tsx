29b1183cb690bd2a0a83cfb40faf4c14fd7a3636
Rifky Alfarez <207006052@student.unsil.ac.id>
2024-06-06 13:34:01 +0700

    refactor: lets connect sections

--- src/components/contact/social-media.tsx
+++ src/components/contact/social-media.tsx
@@ -1,3 +1,4 @@
+import { email, github, instagram, linkedin, whatsapp } from '@/constants';
 import Link from 'next/link';
 import {
   FaEnvelope,
@@ -10,46 +11,61 @@ import {
 export default function LetsConnect() {
   return (
     <div className="mt-4 grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-4">
-      <Link
-        href="/"
-        target="_blank"
-        className="px-4 py-2 rounded-md bg-blue-700 text-white flex items-center justify-center shadow dark:shadow-gray-500 gap-2 text-sm md:text-base font-semibold trasition-color duration-300 ease-in-out hover:bg-blue-500"
-      >
-        <FaLinkedin size={20} />
-        Linkedin
-      </Link>
-      <Link
-        href="/"
-        target="_blank"
-        className="px-4 py-2 rounded-md bg-gray-700 text-white flex items-center justify-center shadow dark:shadow-gray-500  gap-2 text-sm md:text-base font-semibold trasition-color duration-300 ease-in-out hover:bg-gray-500"
-      >
-        <FaGithub size={20} />
-        Github
-      </Link>
-      <Link
-        href="/"
-        target="_blank"
-        className="px-4 py-2 rounded-md bg-red-700 text-white flex items-center justify-center shadow dark:shadow-gray-500  gap-2 text-sm md:text-base font-semibold trasition-color duration-300 ease-in-out hover:bg-red-500"
-      >
-        <FaEnvelope size={20} />
-        Email
-      </Link>
-      <Link
-        href="/"
-        target="_blank"
-        className="px-4 py-2 rounded-md bg-green-700 text-white flex items-center justify-center shadow dark:shadow-gray-500  gap-2 text-sm md:text-base font-semibold trasition-color duration-300 ease-in-out hover:bg-green-500"
-      >
-        <FaWhatsapp size={20} />
-        WhatsApp
-      </Link>
-      <Link
-        href="/"
-        target="_blank"
-        className="px-4 py-2 rounded-md bg-rose-700 text-white flex items-center justify-center shadow dark:shadow-gray-500  gap-2 text-sm md:text-base font-semibold trasition-color duration-300 ease-in-out hover:bg-rose-500"
-      >
-        <FaInstagram size={20} />
-        Instagram
-      </Link>
+      <SocialMedia
+        href={linkedin}
+        color="bg-blue-700 hover:bg-blue-500"
+        Icon={FaLinkedin}
+        name="Linkedin"
+      />
+      <SocialMedia
+        href={github}
+        color="bg-gray-700 hover:bg-gray-500"
+        Icon={FaGithub}
+        name="Github"
+      />
+      <SocialMedia
+        href={email}
+        color="bg-red-700 hover:bg-red-500"
+        Icon={FaEnvelope}
+        name="Email"
+      />
+      <SocialMedia
+        href={whatsapp}
+        color="bg-green-700 hover:bg-green-500"
+        Icon={FaWhatsapp}
+        name="Whatsapp"
+      />
+      <SocialMedia
+        href={instagram}
+        color="bg-rose-700 hover:bg-rose-500"
+        Icon={FaInstagram}
+        name="Instagram"
+      />
     </div>
+  );
+}
+
+export function SocialMedia({
+  href,
+  color,
+  Icon,
+  name,
+}: {
+  href: string;
+  color: string;
+  Icon: React.ElementType;
+  name: string;
+}) {
+  return (
+    <>
+      <Link
+        href={href}
+        target="_blank"
+        className={`px-4 py-2 rounded-md text-white flex items-center justify-center shadow dark:shadow-gray-500 gap-2 text-sm md:text-base font-semibold trasition-color duration-300 ease-in-out ${color}`}
+      >
+        <Icon size={20} />
+        {name}
+      </Link>
+    </>
   );
 }
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -30,3 +30,10 @@ export const navLinks = [
 
 export const selfDescription =
   'A third-year student majoring in informatics engineering with an interest in Frontend Development. Mastering several frontend technologies such as Tailwind CSS, React.js, Next.js, and others. Also, have a basic understanding of backend development such as Express.js and MongoDB. A high spirit of learning and a desire to keep growing makes me always ready to face new challenges.';
+
+export const linkedin = 'https://www.linkedin.com/in/rifkyalfarez/';
+export const github = 'https://github.com/rfkyalf';
+export const email = 'mailto:alfarez2325@gmail.com';
+export const whatsapp =
+  'https://api.whatsapp.com/send/?phone=6289650104144&text&type=phone_number&app_absent=0';
+export const instagram = 'https://www.instagram.com/rfkyalf/';

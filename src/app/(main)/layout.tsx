c0f6efcc5cfd2090eed884df4aa86d7b82b678cc
Rifky Alfarez <207006052@student.unsil.ac.id>
2024-06-08 12:07:13 +0700

    add gif on not found page

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,13 +1,19 @@
+'use client';
+
 import Navbar from '@/components/navbar';
+import { usePathname } from 'next/navigation';
 
 export default function MainLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const pathname = usePathname();
+  const isProjectDetailPage = pathname.startsWith('/projects/');
+
   return (
     <div>
-      <Navbar />
+      {!isProjectDetailPage && <Navbar />}
       {children}
     </div>
   );
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,19 +4,20 @@ import Link from 'next/link';
 export default function NotFound() {
   return (
     <div className="flex items-center justify-center h-screen">
-      <div className="flex items-center backdrop-blur-xl p-8 rounded-xl">
+      <div className="flex items-center p-8 rounded-xl gap-x-4">
         <div>
           <Image
-            src="/images/not-found.png"
+            src="/gif/not-found.gif"
             alt="404"
-            width={300}
-            height={300}
+            width={1000}
+            height={1000}
+            className="w-full h-32 md:w-full md:h-64"
             priority
           />
         </div>
         <div>
           <h1 className="text-2xl md:text-5xl font-bold mb-4">Oops...</h1>
-          <h2 className="text-xl md:text-4xl font-bold">404 Not Found</h2>
+          <h2 className="text-xl md:text-4xl font-bold">Page Not Found</h2>
           <p className="text-sm md:text-xl mb-8">
             The page you are looking for does not exist
           </p>
